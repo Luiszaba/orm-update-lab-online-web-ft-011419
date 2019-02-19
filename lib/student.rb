35142bc042452bb93e68d1036b48abcb049d55a9
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:09:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment.rb"
 class Student
   
   attr_accessor :name, :grade
-  attr_writer :id
+  attr_reader :id
 
 
 def initialize(id = nil, name, grade)
@@ -17,7 +17,7 @@ end
 
 def self.create_table
   sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
+    CREATE TABLE IF NOT EXIST students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
