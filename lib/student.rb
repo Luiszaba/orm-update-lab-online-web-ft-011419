f24de7d5dee58aefddbb1d913cc91c6307925860
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:08:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_writer :id
 
 
 def initialize(id = nil, name, grade)
