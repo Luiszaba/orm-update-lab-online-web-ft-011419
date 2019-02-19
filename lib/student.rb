64d2f2c5b276b1e87b0b946346fc8fba85615f08
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:10:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ end
 
 def self.create_table
   sql = <<-SQL
-    CREATE TABLE IF NOT EXISTSstudents (
+    CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
