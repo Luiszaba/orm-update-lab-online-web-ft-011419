6ea3bf8f44e72308ecb89ee8f523b16b4a9158b9
Luis Zabala <luis.zabala076@gmail.com>
2019-02-19 23:29:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,9 +68,7 @@ end
 
 
 def update
-  sql = <<-SQL
-  UPDATE students SET name = ?, grade = ?, WHERE id = ?
-  SQL
+  sql = "UPDATE students SET name = ? grade = ? WHERE id = ?"
   DB[:conn].execute(sql, self.name, self.grade, self.id)
 end
 end
